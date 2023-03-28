cfa537cd40a89476e0ee4cf8317066c8a4b15fb7
KyriakiPot <37340794+KyriakiPot@users.noreply.github.com>
2023-03-28 22:19:37 +0300

    Update createRelationships.py

--- createRelationships.py
+++ createRelationships.py
@@ -2,7 +2,7 @@ import pandas as pd
 from py2neo import Graph, Node
 import math
 
-df = pd.read_csv("C:/Users/Paris/Documents/WebMining/WDM1.csv", low_memory=False)
+df = pd.read_csv("C:/Users/Paris/Documents/WebMining/WDM1.csv", low_memory=False).astype(str)
 # Connect to the Neo4j database
 graph = Graph("bolt://localhost:7687",auth=("neo4j","34599513"))
 
@@ -12,7 +12,7 @@ for index, row in df.iterrows():
     tweet = {}
     tweet["id"] = row.get(f"includes.tweets.{0}.id")
     print(index)
-    if all(tweet.values()) and not all(math.isnan(v) for v in tweet.values()):  # only add non-empty and non-nan tweets
+    if all(tweet.values()) and not all(pd.isna(v) and  pd.isnull(v) and v != "nan" for v in tweet.values()):  # only add non-empty and non-nan tweets
         for j in range(0,25):
             tag = row.get(f"includes.tweets.0.entities.hashtags.{j}.tag")
             if(not pd.isna(tag) and not pd.isnull(tag)):
@@ -24,11 +24,11 @@ for index, row in df.iterrows():
             if(not pd.isna(url) and not pd.isnull(url)):
                 urlRel = graph.run("MATCH (l: Link),(t: Tweet) WHERE l.url = $url1 and t.id = $tweetId CREATE (t)-[r:HAS_URL]->(l) return count(r)",url1 = url, tweetId = tweet["id"]).data()
 
-   # Get the users from the row
+#    # Get the users from the row
     user = {}
     user["id"] = row.get(f"includes.users.{0}.id")
 
-    if all(user.values()) and not all(math.isnan(v) for v in user.values()):  # only add non-empty and non-nan tweets
+    if all(user.values()) and not all(pd.isna(v) and  pd.isnull(v) and v != "nan" for v in user.values()):  # only add non-empty and non-nan tweets
         for j in range(0,13):
             tag = row.get(f"includes.users.0.entities.description.hashtags.{j}.tag")
             if(not pd.isna(tag) and not pd.isnull(tag)):
@@ -42,13 +42,13 @@ for index, row in df.iterrows():
 
         for j in range(0,6):
             mentions_username = row.get(f"data.entities.mentions.{j}.username")
-            mentions_id = row.get(f"data.entities.mentions.{j}.id")
-            if(not pd.isna(mentions_id) and not pd.isnull(mentions_id)):
-                if(not pd.isna(mentions_username) and not pd.isnull(mentions_username)):
-                    mentionCreate = graph.run("MERGE (u:User {id: $id1}) ON CREATE SET u.id = $id1, u.username = $name1 RETURN u",id1 = str(mentions_id), name1 = str(mentions_username)).data()
-                else:
-                    mentionCreate = graph.run("MERGE (u:User {id: $id1}) ON CREATE SET u.id = $id1 RETURN u;",id1 = str(mentions_id)).data()
-                mentionRel = graph.run("CREATE (u1:User {id:$user1Id})-[r:MENTIONS]->(u2:User {id:$user2Id}) return count(r)",user1Id = user["id"], user2Id = mentions_id).data()
+            mentions_id = row[f"data.entities.mentions.{j}.id"]
+            if(not pd.isna(mentions_id) and not pd.isnull(mentions_id) and mentions_id != "nan"):
+                userExists = graph.run("MATCH (u:User {id: $id1}) return count(u.id)",id1 = mentions_id).data()
+                if(userExists[0]["count(u.id)"] == 0):
+                    node = Node("User", id=str(mentions_id), username=str(mentions_username))
+                    graph.merge(node, "User", "id")               
+                mentionRel = graph.run("MATCH (u1:User),(u2:User) WHERE u1.id = $user1Id and u2.id = $user2Id MERGE (u1)-[r:MENTIONS]->(u2) return count(r)",user1Id = user["id"], user2Id = mentions_id).data()
 
 print("tweet begin")
 graph.run("MATCH (u: User),(t: Tweet) WHERE u.id = t.author_id and t.type<> 'retweeted' and t.type<> 'quoted' and t.type<> 'replied_to' CREATE (u)-[r:TWEETED]->(t) return count(r)")
